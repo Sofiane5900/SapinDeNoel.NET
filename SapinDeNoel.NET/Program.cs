11e06c966e8e3302a0c4a26e2b2bd9de588c06ad
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2024-12-20 15:36:31 +0100

    ajout des boules random

--- SapinDeNoel.NET/Program.cs
+++ SapinDeNoel.NET/Program.cs
@@ -1,11 +1,19 @@
-﻿char feuille = '*';
+﻿using System.Runtime.CompilerServices;
+
+char feuille = '*';
+char boule = '0';
 string tronc = "|||";
+string[] colors = new string[2]
+{
+    "Yellow",
+    "Green",
+};
 
 // Taille du sapin, saisie 
 Console.Write("Choisiez la taille du sapin : ");
 int tailleSapin;
 bool successSapin = int.TryParse(Console.ReadLine(), out tailleSapin);
-if (!successSapin || tailleSapin < 1 || tailleSapin > 30)
+if (!successSapin || tailleSapin <= 1 || tailleSapin > 30)
 {
     Console.WriteLine("Veuillez entrer une saisie correcte (entre 1 et 30)");
 }
@@ -14,6 +22,7 @@ else
     DessinerFeuilles(tailleSapin);
     DessinerTronc(tailleSapin);
 }
+
 
 // Fonction DessinerTronc
 void DessinerTronc(int tailleSapin) // 
@@ -27,18 +36,38 @@ void DessinerTronc(int tailleSapin) //
         Console.SetCursorPosition(centerPosition, Console.CursorTop);
         Console.WriteLine(tronc); // J'affiche des troncs en boucle
     }
+
 }
 
 // Fonction DessinerFeuilles
 void DessinerFeuilles(int tailleSapin)
 {
-    int consoleWidth = Console.WindowWidth;  // Je déclare la largeur de la console
+    int consoleWidth = Console.WindowWidth;  // Je déclare un int de la largeur de la console
+    Random random = new Random();
+
 
     for (int i = 1; i <= tailleSapin; i++) // Tant que index est inférieur a la saisie tailleSapin, 
     {
-        string branche= new String(feuille, i * 2 - 1); // Formule pour (n * 2 - 1) suite arithémtique, génere une suite de nombre impair (askip)
-        int centerPosition = (consoleWidth - branche.Length) / 2; 
+        string branche = " ";
+
+        for (int j = 1;  j < (i * 2 - 1); j++) // Formule pour calculer suite de nombe impair (askip)
+        {
+        
+        if (random.NextDouble() < 0.1) 
+        {
+                branche += boule;
+        }
+        else 
+        {
+                branche += feuille;
+                Console.ResetColor();
+            }
+        }
+        int centerPosition = (consoleWidth - branche.Length) / 2;
         Console.SetCursorPosition(centerPosition, Console.CursorTop);
         Console.WriteLine(branche);
+
     }
+
+
 }
