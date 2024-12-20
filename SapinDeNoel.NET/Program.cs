ab24aabd65e3cb6d6a24685481c63a157d2e0e4d
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2024-12-20 14:02:49 +0100

    Ajoutez des fichiers projet.

---
+++ SapinDeNoel.NET/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string[] largeurTronc = new string[1] { "|||" };
+List<string> listFeuilles = new List<string>();
+char feuille = '*';
+
+Console.Write("Choisiez la taille du sapin : ");
+int tailleSapin;
+bool successSapin = int.TryParse(Console.ReadLine(), out tailleSapin);
+if(!successSapin || tailleSapin < 0 || tailleSapin > 30 )
+{
+    Console.WriteLine("Veuillez entrer une saisie correct");
+} else
+{
+    
+}
+
+
+void DessinerFeuilles(string[] largeurTronc)
+{
+    for (int i = 0; i < largeurTronc.Length; i++)   
